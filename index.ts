1c74bec9ef01aab9f396d4de6ebcd93cc0785f8b
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2022-02-15 13:40:42 +0000

    DiscordClient helper

--- index.ts
+++ index.ts
@@ -1,1 +1,32 @@
 import fetch from "node-fetch"
+
+type HTTPMethod = "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "PATCH"
+type HTTPMethodWithData = "POST" | "PUT" | "DELETE" | "PATCH"
+
+class DiscordClient {
+  baseEndpoint = "https://discord.com/api/v9/"
+  token: string
+
+  async apiRequest<Res extends any, Body extends any = never>(
+    path: string,
+    method?: HTTPMethod,
+    data?: Body
+  ): Promise<Res> {
+    const requestOptions: RequestInit = {}
+
+    if (data) {
+      requestOptions.headers["Content-Type"] = "application/jsons"
+      requestOptions.body = JSON.stringify(data)
+    }
+
+    const responseData = (await fetch(
+      this.baseEndpoint + path.replace(/^\//, "")
+    ).then((res) => res.json().catch(() => res.text()))) as Res
+
+    return responseData
+  }
+
+  constructor(token) {
+    this.token = token
+  }
+}
