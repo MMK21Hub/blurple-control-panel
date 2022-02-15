86bec3287a2b4c79c38e1a002449b8934adc5a44
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2022-02-15 14:44:01 +0000

    fs stuff

--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
 import fetch from "node-fetch"
+import { readFile, writeFile } from "fs/promises"
 
 type HTTPMethod = "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "PATCH"
 type HTTPMethodWithData = "POST" | "PUT" | "DELETE" | "PATCH"
+
+interface LocalAccount {
+  name: string
+  id: string
+  token: string
+  aliases: string[]
+}
 
 class DiscordClient {
   baseEndpoint = "https://discord.com/api/v9/"
@@ -30,3 +38,23 @@ class DiscordClient {
     this.token = token
   }
 }
+
+async function loadAccountsFile() {
+  const fileContents = await readFile(accountsFilePath, "utf-8").catch(
+    async (err) => {
+      if (err.code != "ENOENT") throw err
+
+      console.log(
+        `Could not find accounts file at ${accountsFilePath}. Creating a new file.`
+      )
+      await writeFile(accountsFilePath, JSON.stringify([]), "utf-8")
+      return readFile(accountsFilePath, "utf-8")
+    }
+  )
+
+  return JSON.parse(fileContents) as LocalAccount[]
+}
+
+const accountsFilePath = "accounts.json"
+
+const accountsDatabase = await loadAccountsFile()
